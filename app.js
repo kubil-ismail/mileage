198d5ff98f2e1429a8d75f12f097bedeb7f8cceb
Bilkis Ismail <bilkisismail07@gmail.com>
2020-05-07 09:23:08 +0700

    stable function

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-const { runCalculate} = require('./src/functions');
+const runCalculate = require('./src/functions')
 
 let start = {
-    hrs: 10,
-    min: 12,
-    sec: 21
+    hrs: '10',
+    min: '12',
+    sec: '21'
 }
 
 let end = {
-    hrs: 12,
-    min: 00,
-    sec: 00
+    hrs: '12',
+    min: '00',
+    sec: '00'
 }
 
-runCalculate(start, end);
+const run = runCalculate(start, end)
+console.log(`Perjalanan selama pukul ${run.start} - ${run.end} jarak yang di tempuh : ${run.distance}`)
--- src/functions.js
+++ src/functions.js
@@ -1,47 +1,46 @@
-const onFiveMinute = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve(2)
-    }, 30)
-})
-
-const onTenMinute = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve(1)
-    }, 60)
-})
-
-// Convert to Milisecond
-function calculateMiliSec(time) {
-    return ((time.hrs * 60 * 60 + time.min * 60 + time.sec) * 1000);
+// Get Milisecond
+function getMs(time) {
+    return new Date(`2020-01-01T${time.hrs}:${time.min}:${time.sec}`).getTime();
 }
 
-async function calculateMils(start, end) {
-    // Milisecond
-    let startMs = calculateMiliSec(start);
-    let endMs = calculateMiliSec(end);
-
-    // Start speed
-    let acceleration = 3;
-    let speed = 6;
-
-    try {
-        // Run on 5 Minute
-        acceleration += await onFiveMinute
-        startMs += 300000;
-
-        // Loop on 10 minute
-        for (startMs; startMs <= endMs; startMs += 600000) {
-            acceleration += await onTenMinute
-        }
-    } catch (e) {
-        acceleration = 3
-    }
-
-    return (speed + acceleration) * 3.6;
+// Get Time
+function getTime(time) {
+    return `${time.hrs}:${time.min}:${time.sec}`
 }
 
-module.exports = {
-    runCalculate: function (start, end) {
-        calculateMils(start, end).then(result => console.log(`Jarak Tempuh ${result} km`));
+const runCalculate = (start, end) => {
+    // Get Milisecond
+    let startMs = getMs(start) // Start Milisecond
+    let endMs = getMs(end) // End Milisecond
+    let diffMs = (endMs - startMs) / 1000 // Different Milisecond
+
+    // Default variabel
+    let speed = 6
+    let minute = 0
+    let milage = 0
+
+    // Loop until diffMs 0 seconds
+    for (let seconds = 0; seconds <= diffMs; seconds++) {
+        // Convert to minute
+        minute = seconds / 60
+
+        // If 5 minute + 2
+        if (minute === 5) speed += 2
+
+        // On 10 minute + 1
+        if ((minute - 5) % 10 === 0 && minute !== 5) speed++
+
+        milage += speed
     }
-};
+
+    // Return data
+    let data = {
+        start: getTime(start),
+        end: getTime(end),
+        distance: milage / 1000
+    }
+
+    return data
+}
+
+module.exports = runCalculate
